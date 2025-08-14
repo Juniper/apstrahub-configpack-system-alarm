691b396d355517e251e471aca522e6ef95b473c5
rajagopalans <rajagopalans@gmail.com>
2025-08-14 13:39:05 -0400

    Update variable.tf

--- pack/tofu/variable.tf
+++ pack/tofu/variable.tf
@@ -4,10 +4,10 @@ variable "blueprint_id" {
 
 variable "name" {
   type = string
-  description = "Name: Please use only letters, numbers and _"
+  description = "Name: Please use only letters, numbers, - and _"
   validation {
     condition     = !can(regex("[^A-Za-z0-9_-]", var.name))
-    error_message = "The name must only have letters, numbers and _"
+    error_message = "The name must only have letters, numbers, - and _"
   }
   default = "System_Alarm"
 }
